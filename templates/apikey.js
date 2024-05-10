3371d7ff4e37618d9a78b7b2d721586e4e0a8750
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2024-05-10 18:58:26 +0900

    Delete templates/apikey.js

--- templates/apikey.js
+++
@@ -1,5 +0,0 @@
-const wind_key = 'Em-6DOwZTEyvugzsGfxMGg'
-const factory_key = '3p0oKkkZubsYiLkt0HRHbm0+xK44s8ODlw1ImalHJO/zg/TaRDDli4rzvrxzMSP/EChlBFReC3lb/rNmuV7jRw=='
-const db_pw = 'fJ5BiCL67DEmdVhC'
-
-
