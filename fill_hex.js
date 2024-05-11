24324f5e52af4f69abb1c8d9b0b01fe7737dd78d
Jelly1500 <58739539+Jelly1500@users.noreply.github.com>
2024-05-11 16:24:42 +0900

    fill_hex.js 변경
    1. 인자 값을 공장 굴뚝 위경도 값, 전체 범위의 반지름(km), 각 조각간의 간격(km)
    2. 입력한 위경도 값을 중심으로 범위를 그릴 수 있도록 계산

--- fill_hex.js
+++ fill_hex.js
@@ -31,64 +31,62 @@ function kmToLat(km) {
     return km / distanceKmPerDegree;
 }
 
-// hexDist 값은 km이다. 위도에 따라 경도 간 거리가 달라지므로 이를 고려해야 한다.
-// startLat = 시작 위도, startLng = 시작 경도, n = 맨 윗줄에 나열할 헥사곤 개수
-function hexFill(startLat, startLng, n, hexDist) {
+// hexDist는 km이다. 위도에 따라 경도 간 거리가 달라지므로 이를 고려해야 한다.
+// startLat = 공장 굴뚝 위도, startLng = 공장 굴뚝 경도, n = 맨 윗줄에 나열할 헥사곤 개수
+function hexFill(factLat, factLng, areaDist, hexDist) {
     let hexList = [];   // 반환할 위경도 좌표를 저장, 요소의 형식은 [위도, 경도] 
     let precision = 5;  // 소수점 자릿수 
+    let n = areaDist / hexDist; // 맨 윗줄에 들어갈 수 있는 요소의 개수
 
-    let latDist = kmToLat(hexDist); // km단위 직선거리값에 따른 위도 변화량
-    let lngDist = kmToLng(startLat, hexDist);   // km단위 직선거리값에 따른 경도 변화량
+    let cosValue = Math.cos(Math.PI / 6);
+    let sinValue = Math.sin(Math.PI / 6);
+    let cosDist = cosValue * 2 * hexDist;   // 각 위치 간의 경도상 직선거리
+    let sinDist = sinValue * 2 * hexDist;   // 각 위치 간의 위도상 직선거리
+    let latDist = kmToLat(sinDist); // cosDist를 위도 변화량으로 바꾸기
+    let lngDist = kmToLng(factLat, cosDist);    // sinDist를 경도 변화량으로 바꾸기
+    // 공장 굴뚝 위치를 기준으로 범위의 맨 위쪽 왼쪽 좌표를 계산.
+    let startLat = factLat + latDist * n / 2;   
+    let startLng = factLng - lngDist * n / 2;
 
+    
     // 첫번째 줄 채우기
     hexList.push([startLat, startLng]);
     for (let i = 1; i < n; i++) {
-        hexList.push([startLat, startLng + 2 * i * lngDist]);
+        hexList.push([startLat, startLng + lngDist * i]);
     }
+    latDist = kmToLat(sinDist);
+    lngDist = kmToLng(startLat, cosDist);
 
     // 맨 위에서 중간까지
-    let cosValue = 0.0;
-    let sinValue = 0.0;
     for (let i = 0; i < n-1; i++) {
         let width = n+i;    // 이전에 나열한 헥사곤의 개수
         let hexBuf = hexList.slice(-width); // 이전 줄에 나열된 헥사곤 위경도 값 가져오기
-        lngDist = kmToLng(hexBuf[0][0], hexDist);   // 위도 값이 변했으므로 경도 변화량도 업데이트
+        lngDist = kmToLng(hexBuf[0][0], cosDist);   // 위도 값이 변했으므로 경도 변화량도 업데이트
         // 헥사곤 위치값 계산
-        cosValue = roundToPrecision(Math.cos(4 / 6 * (Math.PI * 2)), precision);
-        sinValue = roundToPrecision(Math.sin(4 / 6 * (Math.PI * 2)), precision);
-        let newLat = roundToPrecision(hexBuf[0][0] + 2 * cosValue * latDist, precision);
-        let newLng = roundToPrecision(hexBuf[0][1] + sinValue * lngDist, precision);
+        let newLat = roundToPrecision(hexBuf[0][0] - latDist, precision);
+        let newLng = roundToPrecision(hexBuf[0][1] - lngDist / 2, precision);
         hexList.push([newLat, newLng]); 
-
-        cosValue = roundToPrecision(Math.cos(5 / 6 * (Math.PI * 2)), precision);
-        sinValue = roundToPrecision(Math.sin(5 / 6 * (Math.PI * 2)), precision);
         for (let j = 0; j < width; j++) {
-            newLat = roundToPrecision(hexBuf[j][0] - 2 * cosValue * latDist, precision);
-            newLng = roundToPrecision(hexBuf[j][1] - sinValue * lngDist, precision);
+            newLat = roundToPrecision(hexBuf[j][0] - latDist, precision);
+            newLng = roundToPrecision(hexBuf[j][1] + lngDist / 2, precision);
             hexList.push([newLat, newLng]);
         }
     }
 
     // 중간에서 맨 아래까지
-    cosValue = roundToPrecision(Math.cos(5 / 6 * (Math.PI * 2)), precision);
-    sinValue = roundToPrecision(Math.sin(5 / 6 * (Math.PI * 2)), precision);
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 0; i < n-1; i++) {
         let width = (2 * n - 1) - i;
         let hexBuf = hexList.slice(-width);
-        lngDist = kmToLng(hexBuf[0][0], hexDist);   // 위도 값이 변했으므로 경도 변화량도 업데이트
+        lngDist = kmToLng(hexBuf[0][0], cosDist);   // 위도 값이 변했으므로 경도 변화량도 업데이트
         // 헥사곤 위치값 계산
         for (let j = 0; j < width - 1; j++) {
-            let newLat = roundToPrecision(hexBuf[j][0] - 2 * cosValue * latDist, precision);
-            let newLng = roundToPrecision(hexBuf[j][1] - sinValue * lngDist, precision);
+            let newLat = roundToPrecision(hexBuf[j][0] - latDist, precision);
+            let newLng = roundToPrecision(hexBuf[j][1] + lngDist / 2, precision);
             hexList.push([newLat, newLng]);
         }
     }
     return hexList;
 }
 
-var map = L.map('map').setView([35.1188093910081, 128.96808496718], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+
 
